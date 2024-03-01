56c083cee4e72250f9e4fda689132ad783fa10de
Nicolas Gonot <gg_nicolas@hotmail.com>
2024-03-01 16:03:51 +0100

    up

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -12,41 +12,48 @@ const fade = () => (
         showThumbs={false}
         // axis="vertical"
     >
-        {/* ------------------------------------------------------------------------------------------- */}
+        {/* ---------------------------------------------------------------------------------------- */}
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
                 className="carousel-img"
                 src="/images/gamer.jpg"
                 alt="Gaming"
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
             />
             <div className="centered-label">Gaming</div>
         </div>
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+                className="carousel-img"
+                src="/images/Fallout.jpg"
+                alt="Fallout images"
+            />
+            {/* <div className="centered-label">Label 4</div> */}
+        </div>
+
+        <div style={{ position: "relative" }}>
+            <img
                 className="carousel-img"
                 src="/images/Cyberpunk2077.jpg"
                 alt="Cyberpunk2077 images"
             />
+            {/* <div className="centered-label">Label 4</div> */}
         </div>
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
                 className="carousel-img"
                 src="/images/Warzone.jpg"
                 alt="Warzone images"
             />
+            {/* <div className="centered-label">Label 4</div> */}
         </div>
 
-        {/* ------------------------------------------------------------------------------------------- */}
+        {/* ---------------------------------------------------------------------------------------- */}
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
                 className="carousel-img"
                 src="/images/china1.jpg"
                 alt="images 2"
@@ -54,29 +61,10 @@ const fade = () => (
             <div className="centered-label">Travel</div>
         </div>
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
-            <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
-                className="carousel-img"
-                src="/images/boudah.jpg"
-                alt="Warzone images"
-            />
-        </div>
+        {/* ---------------------------------------------------------------------------------------- */}
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
-                className="carousel-img"
-                src="/images/plage.jpg"
-                alt="Warzone images"
-            />
-        </div>
-
-        {/* ------------------------------------------------------------------------------------------- */}
-
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
-            <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
                 className="carousel-img"
                 src="/images/china2.jpg"
                 alt="images 3"
@@ -84,14 +72,16 @@ const fade = () => (
             <div className="centered-label">Discovery</div>
         </div>
 
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ position: "relative" }}>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "auto" }}
                 className="carousel-img"
                 src="/images/china3.jpg"
                 alt="images 4"
             />
+            {/* <div className="centered-label">Label 4</div> */}
         </div>
+
+        {/* ---------------------------------------------------------------------------------------- */}
     </Carousel>
 );
 
